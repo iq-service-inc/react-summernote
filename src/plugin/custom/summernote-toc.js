107145a83e0fc72dee99d4b066a0a6c8e8703b1c
Vanessa Wang <vanessa80332@gmail.com>
2021-01-15 18:24:42 +0800

    修改 toc plugin classname 新增 prefix

--- src/plugin/custom/summernote-toc.js
+++ src/plugin/custom/summernote-toc.js
@@ -20,7 +20,8 @@
 }(function ($) {
     $.extend($.summernote.options, {
         toc: {
-            selector: null
+            selector: null,
+            prefix: null
         }
     });
     $.extend($.summernote.plugins, {
@@ -39,6 +40,8 @@
                 options = context.options,
                 lang = options.langInfo;
 
+            var prefix = options.toc.prefix || 'summernote'
+
             var isMac = navigator.appVersion.indexOf('Mac') > -1
             var shortcut = 'CTRL+SHIFT+A';
                 
@@ -56,10 +59,10 @@
             </svg>`
 
             this.initialize = function () {
-                var css = $('<style>').html(`.toc-anchor{position:relative;}.toc-anchor::before{content:"#";opacity:0.6;position:absolute;left:-0.7em;visibility:hidden;}.toc-anchor:hover::before{visibility:visible;}.toc-mark{background-color:yellow;}.toc-mark::before{visibility:visible;}.toc-form-container{max-height:700px;overflow-y:auto;}.toc-form-group{display:flex;}.toc-form-group>.toc-data-id{width:50%;}.toc-form-group>.toc-data-text{width:50%;}`)
+                var css = $('<style>').html(`.${prefix}-toc-anchor{position:relative;}.${prefix}-toc-anchor::before{content:"#";opacity:0.6;position:absolute;left:-0.7em;visibility:hidden;}.${prefix}-toc-anchor:hover::before{visibility:visible;}.${prefix}-toc-mark{background-color:yellow;}.${prefix}-toc-mark::before{visibility:visible;}.${prefix}-toc-form-container{max-height:700px;overflow-y:auto;}.${prefix}-toc-form-group{display:flex;}.${prefix}-toc-form-group>.${prefix}-toc-data-id{width:50%;}.${prefix}-toc-form-group>.${prefix}-toc-data-text{width:50%;}`)
                 $(document.head).append(css)
 
-                this.$toc = $(`<div class="toc"></div>`)
+                this.$toc = $(`<div class="${prefix}-toc"></div>`)
                 if (!!options.toc.selector) {
                     $(options.toc.selector).append(this.$toc);
                 }
@@ -71,12 +74,12 @@
                     title: `${lang.toc.editAnchortext}`,
                     fade: options.dialogsFade,
                     body: `
-                    <div class="form-group toc-form-group">
+                    <div class="form-group ${prefix}-toc-form-group">
                         <b>${lang.toc.message}</b>
                     </div>`,
-                    footer: `<button href="#" class="btn btn-primary note-toc-btn">${lang.toc.addtoc}</button>`,
+                    footer: `<button href="#" class="btn btn-primary ${prefix}-toc-btn">${lang.toc.addtoc}</button>`,
                 }).render().appendTo(options.container);
-                this.$dialog.find('.modal-body').addClass('toc-form-container')
+                this.$dialog.find('.modal-body').addClass(`${prefix}-toc-form-container`)
 
                 $editable.css('padding', '1.5rem')
                 $editingArea.on('keydown', function (e) {
@@ -92,20 +95,20 @@
 
             context.memo('button.markAnchor', function () {
                 return ui.button({
-                    className: 'btn-mark-anchor',
-                    contents: `<i class="note-icon mark-anchor"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars-fill" viewBox="0 0 16 16">
+                    className: `${prefix}-btn-mark-anchor`,
+                    contents: `<i class="note-icon ${prefix}-mark-anchor"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars-fill" viewBox="0 0 16 16">
                     <path d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1h-1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4h4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14H1zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14H9zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5V3z"/>
                   </svg></i>`,
                     tooltip: lang.toc.markAnchor,
                     click: function (event) {
                         var $target = $(event.target).closest('button')
-                        if ($target.find('.toc-mark').length) {
-                            $target.find('.mark-anchor').removeClass('toc-mark')
-                            $editable.find('.toc-anchor[id]').removeClass('toc-mark')
+                        if ($target.find(`.${prefix}-toc-mark`).length) {
+                            $target.find(`.${prefix}-mark-anchor`).removeClass(`${prefix}-toc-mark`)
+                            $editable.find(`.${prefix}-toc-anchor[id]`).removeClass(`${prefix}-toc-mark`)
                         }
                         else {
-                            $target.find('.mark-anchor').addClass('toc-mark')
-                            $editable.find('.toc-anchor[id]').addClass('toc-mark')
+                            $target.find(`.${prefix}-mark-anchor`).addClass(`${prefix}-toc-mark`)
+                            $editable.find(`.${prefix}-toc-anchor[id]`).addClass(`${prefix}-toc-mark`)
                         }
                     },
                 }).render();
@@ -123,27 +126,27 @@
                     id = id.replace(/[\W\-]/g, '-')
                     if (id.length == 0) return
                     if ($(node).attr('id') === id) {
-                        $(node).toggleClass('toc-anchor')
+                        $(node).toggleClass(`${prefix}-toc-anchor`)
                     }
                     else {
                         var index = 0
                         while ($editable.find(`#${id}${index || ''}`).length) {
                             index += 1
                         }
-                        $(node).attr('id', `${id}${index || ''}`).toggleClass('toc-anchor').data('text', id)
+                        $(node).attr('id', `${id}${index || ''}`).toggleClass(`${prefix}-toc-anchor`).data('text', id)
                     }
-                    if ($toolbar.find('.btn-mark-anchor .toc-mark').length && $(node).hasClass('toc-anchor')) {
-                        $(node).addClass('toc-mark')
+                    if ($toolbar.find(`.${prefix}-btn-mark-anchor .${prefix}-toc-mark`).length && $(node).hasClass(`${prefix}-toc-anchor`)) {
+                        $(node).addClass(`${prefix}-toc-mark`)
                     }
                     else {
-                        $(node).removeClass('toc-mark')
+                        $(node).removeClass(`${prefix}-toc-mark`)
                     }
                 }
             }
 
             context.memo('button.anchor', function () {
                 return ui.button({
-                    className: 'anchor',
+                    className: `${prefix}-anchor`,
                     contents: `<i class="note-icon">#</i>`,
                     tooltip: `${lang.toc.insertanchor} (${shortcut})`,
                     click: function (event) {
@@ -156,42 +159,42 @@
                 ui.showDialog(this.$dialog)
 
                 var $body = self.$dialog.find('.modal-body')
-                var anchors = $editor.find('.toc-anchor[id]')
+                var anchors = $editor.find(`.${prefix}-toc-anchor[id]`)
                 if (anchors.length == 0) {
                     $body.html(`
-                    <div class="form-group toc-form-group">
+                    <div class="form-group ${prefix}-toc-form-group">
                         <b>${lang.toc.message}</b>
                     </div>`)
                     return
                 }
                 // reset dialog body
                 $body.html(`
-                <div class="form-group toc-form-group">
-                    <b class="toc-data-id">id</b>
-                    <b class="toc-data-text">${lang.toc.anchortext}</b>
+                <div class="form-group ${prefix}-toc-form-group">
+                    <b class="${prefix}-toc-data-id">id</b>
+                    <b class="${prefix}-toc-data-text">${lang.toc.anchortext}</b>
                 </div>`)
                 anchors.each((i, d) => {
                     var id = $(d).attr('id'),
                         text = $(d).data('text')
 
-                    var label = $('<label>').text(id).addClass('toc-data-id')
-                    var input = $('<input>').attr('value', text).attr('id', id).addClass('toc-data-text')
-                    var div = $('<div>').addClass(['form-group', 'toc-form-group']).append(label).append(input)
+                    var label = $('<label>').text(id).addClass(`${prefix}-toc-data-id`)
+                    var input = $('<input>').attr('value', text).attr('id', id).addClass(`${prefix}-toc-data-text`)
+                    var div = $('<div>').addClass(['form-group', `${prefix}-toc-form-group`]).append(label).append(input)
                     $body.append(div)
                 })
 
-                var $btn = self.$dialog.find('.modal-footer .note-toc-btn')
+                var $btn = self.$dialog.find(`.modal-footer .${prefix}-toc-btn`)
                 $btn.click(function (event) {
                     ui.hideDialog(self.$dialog)
                     // reset toc
                     self.$toc.html('')
-                    var input = self.$dialog.find('.toc-data-text').not('b')
+                    var input = self.$dialog.find(`.${prefix}-toc-data-text`).not('b')
                     input.each((i, d) => {
                         let text = $(d).val(),
                             id = $(d).attr('id')
     
                         // update anchor text
-                        $editor.find(`.toc-anchor#${id}`).data('text', text)
+                        $editor.find(`.${prefix}-toc-anchor#${id}`).data('text', text)
 
                         // 產生 TOC
                         var anchor = $('<a>').attr('href', '#' + id)
@@ -208,7 +211,7 @@
 
             context.memo('button.toc', function () {
                 return ui.button({
-                    className: 'toc',
+                    className: `${prefix}-toc`,
                     contents: `<i class="note-icon">${linkicon}</i>`,
                     tooltip: lang.toc.addtoc,
                     click: function (event) {
